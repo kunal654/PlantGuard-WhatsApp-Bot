d673252e9112a1eb0506640bdc68b20e5ad29d48
Kunal Gautam <136215385+kunal654@users.noreply.github.com>
2024-12-13 13:29:22 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 from dotenv import load_dotenv
 
 import requests
-url = "https://web-production-0cb7.up.railway.app/predict/"
+url = "https://web-production-904d.up.railway.app/predict/"
 
 def detect(file):
     file = {"file": open(file,'rb')}
